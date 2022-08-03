97031eb506143878225f59401d52cdaab5c63a2a
manishkumarmandane <bm0369@gmail.com>
2022-08-03 22:21:06 +0100

    table9

---
+++ TableNine.java
@@ -0,0 +1,20 @@
+package WhileLoopTable9;
+
+public class TableNine
+{
+    public static void main(String[] args)
+    {
+      int i=1,j=9,k;
+      while ( i<=10)
+      {
+
+
+              k = j* i;
+
+              System.out.println(j + "*"+ i + " = " + k);
+              i++;
+          }
+
+    }
+
+}
